12de2fcc02bb0ce1a12c58212c610ffd77e67858
Radim Sevcik <radim.sevcik@goloveparametry.cz>
2013-03-29 18:45:53 +0100

    Averages for whole league added and display of a mean filtered by min 3 occurences

--- code/squadAge.R
+++ code/squadAge.R
@@ -1,12 +1,11 @@
 # TODO
-# **Get Average for the whole league
-# **Add counts to data frame gr
+# DONE **Get Average for the whole league
+# **Add counts to data frame df.agr
 
 # Load required package(s)
 
 require("ggplot2")
 require("XML")
-
 
 # List of dates when seasons started (hardcoded)
 
@@ -48,14 +47,17 @@ df <- data.frame(
 ## FETCH
 
 # Fetch URLs for every team in current season (named vector)
+
+# Fetch URL
 url <- xpathSApply(
-  htmlParse("http://www.gambrinusliga.cz/"),
+  htmlParse("http://www.gambrinusliga.cz/index.php"),
   "/html/body/header/div/div[2]/div[1]/div[1]/ul/li/a", 
   function(x) c(paste("http://www.gambrinusliga.cz", xmlAttrs(x)[["href"]], sep = ""))
   )
 
+# Fetch team name
 names(url) <- xpathSApply(
-  htmlParse("http://www.gambrinusliga.cz/"),
+  htmlParse("http://www.gambrinusliga.cz/index.php", encoding="UTF-8"),
   "/html/body/header/div/div[2]/div[1]/div[1]/ul/li/a/img", 
   function(x) c(xmlAttrs(x)[["title"]])
 )
@@ -113,78 +115,101 @@ names(df) <- c(
   "dateSeasonStart"  
   )
 
-# Calculate players age at the start of the season
+# Calculate players age at the start of the given season
 
 df$playerAgeYrs <- as.numeric(difftime(df$dateSeasonStart, df$dateBorn, units = "days")/365)
 
-# Total average
-
-avg <- aggregate(dff$playerAgeYrs, by=list(dff$season), FUN = mean, na.rm = T)
-avg$team <- "total"
-names(avg) <- c("season", "team", "avgSquadAge")
-avg <- avg[ , c(1, 3, 2) ]
-
 # FILTER
 
+# Position filter
 filter <- list("1" = "No Filter", 
                "2" = "Defense",
                "3" = "Midfield",
                "4" = "Attack")
+
+# Set position filter HERE 
 filterOption <- names(filter)[1]
 
-# Filter by position (dff: data frame filtered)
-
-dff <-  if(filterOption == "2") df[df$position == "O",] else if(filterOption == "3") dff <- df[df$position == "Z",] else if(filterOption == "4") df[df$position == "U",] else df
+df.f <-  if(filterOption == "2") df[df$position == "O",] else if(filterOption == "3") dff <- df[df$position == "Z",] else if(filterOption == "4") df[df$position == "U",] else df
 
 # Filter by team
 
-# AC Sparta Praha and FC Viktoria Plzen
+# # AC Sparta Praha and FC Viktoria Plzen
+# [1] "club-fkslov" "club-fkprib" "club-acsppr" "club-fcbaos" "club-fchrkr" "club-fkslli"
+# [7] "club-fcvipl" "club-fcvyji" "club-fczbbr" "club-fkbaja" "club-duklap" "club-fkmlbo"
+# [13] "club-fktepl" "club-skdyce" "club-sksiol" "club-skslpr"
+df.f <-  df.f[df.f$team == levels(df.f$team)[3] | df.f$team == levels(df.f$team)[7], ]
+df.f$team <- factor(df.f$team)
 
-dff <-  dff[dff$team == "club-acsppr" | dff$team == "club-fcvipl", ]
+# Total average
+
+df.all <- aggregate(df.f$playerAgeYrs, by=list(df.f$season), FUN = mean, na.rm = T)
+df.all$team <- "All"
+df.all <- df.all[ , c(1, 3, 2) ]
+names(df.all) <- c("season", "team", "avgSquadAge")
+df.all$freq <- as.data.frame(table(df$season))[, 2]
 
 
 ## AGGREGATE
 
 # Table aggregated means of squad age in days by club and season
-gr <- aggregate(dff$playerAgeYrs, 
-                by = list(dff$season, dff$team), 
+df.agr <- aggregate(df.f$playerAgeYrs, 
+                by = list(df.f$season, df.f$team), 
                 mean, 
                 na.rm = T)
 
-colnames(gr) <- c(
+colnames(df.agr) <- c(
   "season",
   "team",
   "avgSquadAge"
 )
 
-#levels(gr$season) <- rev(levels(gr$season)) # reverse levels
-gr$key <- paste(gr$season, gr$team, "")
+df.agr$key <- paste(df.agr$season, df.agr$team, "")
+
+# Merge with player counts
+df.freq <- as.data.frame(table(df.f$team, df.f$season))
+colnames(df.freq) <- c(
+  "team",
+  "season",
+  "freq"
+)
+df.freq$key <- paste(df.freq$season, df.freq$team, "")
+df.agr <- merge(x = df.freq, y = df.agr, by = "key", all.y = T)[, c(1,2,3,4,7)]
+
+colnames(df.agr) <- c(
+  "key",
+  "team",
+  "season",
+  "freq",
+  "avgSquadAge"
+)
 
 ## REFINE
 
 # Full data frame (in case of missing seasons)
-df1 <- expand.grid(season = seq(1994, 2013), team = levels(gr$team))
-df1$key <- paste(df1$season, df1$team, "")
+df.1 <- expand.grid(season = seq(1994, 2013), team = levels(df.agr$team))
+df.1$key <- paste(df.1$season, df.1$team, "")
 
 # Join full data frame with mean
-df1 <- merge(x = df1, y = gr, by = "key", all.x = T)[, c(2, 3, 6)]
-colnames(df1) <- c("season", "team", "avgSquadAge")
+df.1 <- merge(x = df.1, y = df.agr, by = "key", all.x = T)[, c(2, 3, 6, 7)]
+colnames(df.1) <- c("season", "team", "freq", "avgSquadAge")
 
-df1 <- rbind(df1, avg)
+# Add rows with totals
+df.1 <- rbind(df.1, df.all)
 
 
 # VISUALIZE
 
-cbPalette <- c("#D55E00","#0072B2", "#999999") # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+cbPalette <- c("#D55E00", "#0072B2", "#999999") # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
 
-d <- ggplot(df1[!is.na(df1$avgSquadAge), ], aes(x=as.factor(season), y=avgSquadAge, group=team, colour=team )) + 
-        geom_point(position="dodge", stat = "identity") +
-        geom_line(data=df1[!is.na(df1$avgSquadAge),])
+d <- ggplot(data = df.1[!is.na(df.1$avgSquadAge) & df.1$freq > 2, ], aes(x = as.factor(season), y = avgSquadAge, group = team, colour = team)) + 
+  geom_point(position = "dodge", stat = "identity") +
+  geom_line()
 
 
-d <- d + scale_colour_manual(values=cbPalette)
+d <- d + scale_colour_manual(values = cbPalette)
 d <- d + ylim(20, 30)
-d <- d + labs(list(title = paste("Average Squad Age of", names(url)[1], "and", names(url)[2], "since 1994 (", if(as.integer(filterOption) <= 4) filter[[filterOption]], ")"), 
+d <- d + labs(list(title = paste("Average Squad Age of", df.1$team[1], "and", df.1$team[2], "since 1994 (", if(as.integer(filterOption) <= 4) filter[[filterOption]], ")"), 
                    x = "Season", 
                    y = "Average Squad Age (years)", 
                    colour = "Team" ))
@@ -194,5 +219,4 @@ d
 # dff[dff$team == "fcvik" & dff$season == 1999,]
 # table(dff$position, dff$season, dff$team) filtered data valid since 2000s
 # table(dff$season, dff$team)
-
 
